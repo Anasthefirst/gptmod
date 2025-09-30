7fa85be7c707f191d6203072e7e79b9ee92351e7
Anasthefirst <Anasrefa2@gmail.com>
2025-09-30 09:06:46 -0500

    Create testmod.js

---
+++ testmod.js
@@ -0,0 +1,10 @@
+elements.water.color = "96fffd";
+
+  elements.anas = {
+    color: "#eb4034",
+    behavior: behaviors.GAS,
+    category: "anas",
+    state: "gas",
+    density: 1100,
+    temp: 10000
+  }
